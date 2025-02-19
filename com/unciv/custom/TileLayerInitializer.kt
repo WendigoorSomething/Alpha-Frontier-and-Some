bdcff14f58d56ccef71cdc3ba26ade0209777010
WendigoorSomething <wen56976@gmail.com>
2025-02-19 16:02:23 -0600

    Update TileLayerInitializer.kt

--- com/unciv/custom/TileLayerInitializer.kt
+++ com/unciv/custom/TileLayerInitializer.kt
@@ -3,11 +3,9 @@ package com.unciv.mod.custom
 import com.unciv.ui.components.tilegroups.TileGroup
 import com.unciv.ui.components.tilegroups.layers.TileLayerUnitSprite
 
-// This is an example initializer class for setting up tile layers in your mod
 class TileLayerInitializer {
 
     fun createCustomTileLayer(tileGroup: TileGroup, size: Float): TileLayerUnitSprite {
-        // Return an instance of your custom TileLayerUnitSprite
         return CustomTileLayerUnitSprite(tileGroup, size)
     }
 }
